ecbc8707499133ee7b9479e26b8e9c736b0a72f4
Shreya <shreya.nagesh16@gmail.com>
2019-09-13 08:50:59 -0700

    added the pythagoras code onto the file
    added the pythagoras code onto the file

---
+++ ShreyaLab1/ShreyaLab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ShreyaLab1
+//
+//  Created by Nagesh, Shreya on 9/13/19.
+//  Copyright © 2019 Nagesh, Shreya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
