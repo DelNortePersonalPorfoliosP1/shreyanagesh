c34169b220a819aec7cdc46e66566bee66ed0723
Shreya <shreya.nagesh16@gmail.com>
2019-09-20 08:49:56 -0700

    move files

---
+++ ShreyaLab1/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  pythagoraslab
+//
+//  Created by Nagesh, Shreya on 9/13/19.
+//  Copyright © 2019 Nagesh, Shreya. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+double computeHypotenuse(double a, double b);
+
+#endif /* Header_h */
--- ShreyaLab1/main.c
+++ ShreyaLab1/main.c
@@ -1,15 +1,36 @@
 //
 //  main.c
-//  ShreyaLab1
+//  pythagoraslab
 //
 //  Created by Nagesh, Shreya on 9/13/19.
 //  Copyright © 2019 Nagesh, Shreya. All rights reserved.
 //
 
 #include <stdio.h>
+#include <math.h>
+#include "Header.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+// defining and declaring function
+double computeHypotenuse(double a, double b)
+{
+    double hyp = 0.0;
+    hyp = sqrt(a*a + b*b); // this is the formula to find hypotenuse
+    return hyp;
 }
+
+// main
+int main(int argc, const char * argv[]) {
+    double a, b, c = 0.0; // initializing values
+    printf("Ask for 'a'"); //this is asking for side length a
+    scanf("%lf",&a); // outputs value double
+    printf("Ask for 'b'"); // this is asking for side length b
+    scanf("%lf",&b); // outputs value double
+    
+    
+    c = computeHypotenuse(a, b); // invokes the function
+    
+    
+    printf("hypotenuse value: %lf", c); // this is asking for the hypotenuse value
+    
+}
+
